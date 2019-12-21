5c366dd56066733f61b6b2bf1e05af3ef61db0a6
Melanie Rogoff <mgrogoff@gmail.com>
2019-12-20 23:33:31 -0700

    Finished adding basic weather info for all cities

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -5,7 +5,10 @@ const phxURL = "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&APPID=
 const austURL = "https://api.openweathermap.org/data/2.5/weather?q=Austin&APPID=9be0a529a7dd200677c71e4ba94edd63";
 const chicURL = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=9be0a529a7dd200677c71e4ba94edd63";
 const nyURL = "https://api.openweathermap.org/data/2.5/weather?zip=10004&APPID=9be0a529a7dd200677c71e4ba94edd63";
-
+const orlURL = "https://api.openweathermap.org/data/2.5/weather?q=Orlando&APPID=9be0a529a7dd200677c71e4ba94edd63";
+const sfURL = "https://api.openweathermap.org/data/2.5/weather?zip=94102&APPID=9be0a529a7dd200677c71e4ba94edd63";
+const denURL = "https://api.openweathermap.org/data/2.5/weather?zip=80202&APPID=9be0a529a7dd200677c71e4ba94edd63";
+const ldnURL = "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=9be0a529a7dd200677c71e4ba94edd63";
 
 const searchVal = $("#searcher").val();
 let now = moment(); //Starts the interval when page loads
@@ -49,7 +52,7 @@ $("#austBtn").on('click', function() {
 
         //When clicking the Chicago button ...
 $("#chicBtn").on('click', function() {
-  //Call the AJAX API for Austin
+  //Call the AJAX API for Chicago
       $.ajax({
        url: chicURL,
       method: "GET"
@@ -63,9 +66,69 @@ $("#chicBtn").on('click', function() {
 
 //When clicking the NY button ...
 $("#nyBtn").on('click', function() {
-  //Call the AJAX API for Austin
+  //Call the AJAX API for NY
       $.ajax({
        url: nyURL,
+      method: "GET"
+  }).then(function(response) {
+        //Display city name/date/icon
+        $("#mainstate").append(response.name);
+        $("#mainstate").append(date);
+        $("#mainstate").append("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Weather Icon'>");
+      });
+    });
+
+
+//When clicking the Orlando button ...
+$("#orlandoBtn").on('click', function() {
+  //Call the AJAX API for Orlando
+      $.ajax({
+       url: orlURL,
+      method: "GET"
+  }).then(function(response) {
+        //Display city name/date/icon
+        $("#mainstate").append(response.name);
+        $("#mainstate").append(date);
+        $("#mainstate").append("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Weather Icon'>");
+      });
+    });
+
+
+//When clicking the SF button ...
+$("#sfBtn").on('click', function() {
+  //Call the AJAX API for SF
+      $.ajax({
+       url: sfURL,
+      method: "GET"
+  }).then(function(response) {
+        //Display city name/date/icon
+        $("#mainstate").append(response.name);
+        $("#mainstate").append(date);
+        $("#mainstate").append("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Weather Icon'>");
+      });
+    });
+
+
+
+//When clicking the Denver button ...
+$("#denBtn").on('click', function() {
+  //Call the AJAX API for Denver
+      $.ajax({
+       url: denURL,
+      method: "GET"
+  }).then(function(response) {
+        //Display city name/date/icon
+        $("#mainstate").append(response.name);
+        $("#mainstate").append(date);
+        $("#mainstate").append("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Weather Icon'>");
+      });
+    });
+
+    //When clicking the LDN button ...
+$("#ldnBtn").on('click', function() {
+  //Call the AJAX API for LDN
+      $.ajax({
+       url: ldnURL,
       method: "GET"
   }).then(function(response) {
         //Display city name/date/icon
