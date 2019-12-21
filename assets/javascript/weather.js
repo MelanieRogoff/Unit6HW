ea7dd11e4ab56c3a3d6f17f5e04b112ced7904f0
Melanie Rogoff <mgrogoff@gmail.com>
2019-12-20 20:06:08 -0700

    Displays Phoenix and today's date upon clicking Phoenix

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,6 +3,8 @@ let searchHistory = []; //Empty array for search history
 const queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=9be0a529a7dd200677c71e4ba94edd63";
 const phxURL = "https://api.openweathermap.org/data/2.5/weather?q=Phoenix&APPID=9be0a529a7dd200677c71e4ba94edd63";
 const searchVal = $("#searcher").val();
+let now = moment(); //Starts the interval when page loads
+let date = now.format(" (MM/D/YYYY)"); //Format for current month/day/year
 
 //Pushing the value of the text input into the search history array
 searchHistory.push(searchVal); 
@@ -17,10 +19,10 @@ $("#phxBtn").on('click', function() {
        url: phxURL,
       method: "GET"
   }).then(function(response) {
-        // Console.log to check for it working
-        console.log(phxURL);
-        //Display city name
+        //Display city name/date/icon
         $("#mainstate").append(response.name);
+        $("#mainstate").append(date);
+        $("#mainstate").append(response.weather[0].icon); //Displays text but not icon..
         
       });
     });
